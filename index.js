fb649eb83f3a96ea223e5bffa99d3f22df51cf57
NtandoRaji <2584925@students.wits.ac.za>
2024-04-10 22:13:01 +0200

    edited index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //create cars api using express
 const express = require('express');
-const app = express();
 const cors = require("cors");
+
+const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cors());
